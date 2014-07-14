668aaf135f33362c7da3425c94003b28c2460b63
Xing Chen <x.chen1@ncl.ac.uk>
2014-07-14 12:57:21 +0100

    new files for lesson

---
+++ parfor_sample_code.asv
@@ -0,0 +1,49 @@
+function parfor_sample_code
+%Written by Xing 11/07/14
+
+%Examples of how to use parfor loops, how to split the data processing into
+%distinct stages.
+%Open workers using this command:
+matlabpool open
+
+%20 files of raw data to be processed per day.
+%Total of 10 days.
+files=1:20;
+days=1:10;
+
+%Scenario 1: data files can be processed completely independently of each
+%other.
+totalFiles=length(files)*length(days)
+
+parfor processNumber=1:totalFiles
+    %process the data here:
+    sprintf()
+    dayNumber=floor(processNumber/20)+1;
+    fileNumber=mod(processNumber,20)+1;
+    sprintf('process number %s, file #%s, day %s',num2str(processNumber),num2str(fileNumber),num2str(dayNumber))
+end
+
+
+%Scenario 2: data files can be processed semi-independently of each
+%other- possible to process several file 'numbers' at a time, e.g. file 1
+%from day 1 and file 2 from day 1, but NOT several days simultaneously for
+%a given file number, e.g. file 1 from day 1 and file 1 from day 2.
+totalRuns=length(files)
+
+parfor fileNumber=1:totalRuns
+    %process the data here, in an orderly sequence from day 1 to day 10:
+    for dayCounter=1:length(days)
+        sprintf('file #%s, day %s',num2str(fileNumber),num2str(dayCounter))
+    end
+end
+
+%slow down the processing, so that you can see the order- file numbers may
+%be processed out of sequence, but for a given file number, days will
+%always be processed in sequence:
+parfor fileNumber=1:totalRuns
+    %process the data here, in an orderly sequence from day 1 to day 10:
+    for dayCounter=1:length(days)
+        sprintf('file #%s, day %s',num2str(fileNumber),num2str(dayCounter))
+        pause(1)
+    end
+end
---
+++ plot_figs_sample_code.m
@@ -0,0 +1,142 @@
+function plot_figs_sample_code
+%Written by Xing 13/07/14 to generate example figure, plot markers, draw
+%fitted curve, and export to a file
+
+truncateXLim=1;
+plotDataPoints=1;
+
+%specify styles of lines to plot:
+trainingStageLineStyles=[{':'} {'--'} {'-'}];
+
+%colours- red, green, blue:
+lineCols=[{'r'} {[0/256 100/256 0/256]} {'b'}];%speficy shade of green
+
+%create figure:
+fig1=figure('Color',[1,1,1],'Units','Normalized','Position',[0.12, 0.08, 0.25, 0.8]);%'Position': left, bottom, width, height
+
+sampleContrast=30;
+xDataPoints=[5,10,15,20,22,25,28,32,35,40,45,50,60,90];
+yDataPoints=[0,0.0217,0.0632,0.0526,0.119,0.298,0.386,0.569,0.654,0.899,0.978,1,0.978,0.988];
+
+%plot draw data points in the form of circles:
+if plotDataPoints==1;
+    plot(xDataPoints,yDataPoints,'Marker','o','Color','r','LineStyle','none','MarkerSize',5);
+    hold on%holds the current plot and all axis properties so that subsequent graphing commands add to the existing graph
+end
+
+%perform curve fitting:
+X0=[2 30 0.2 0.1];%initialise parameters
+X=fminsearch(@fit_weibull,X0,[],xDataPoints,yDataPoints,[],'least_square',[1 0 1 0],[20 0 max(yDataPoints) 0],[1 0 0 0],[0 0 0 0]);
+X=[4.36,33.5,0.99,0.0069];
+
+%generate values for plotting fitted curve
+xvals=0:1:xDataPoints(end)+10;
+yvals=1-X(4)-X(3).*exp(-(xvals./X(2)).^X(1));
+
+%draw fitted curve:
+plot(xvals,yvals,'Color','r','LineStyle','-');
+
+sampleContrasts=[20 30 40];
+testContrasts=[5,10,12,15,18,22,25,28,35,45,60,90;5,10,15,22,25,28,32,35,38,45,60,90;5,10,15,25,32,35,38,42,45,50,60,90];
+for sampleContrastInd=1:length(sampleContrasts)
+    testContrast=testContrasts(sampleContrastInd,:);
+    rovingPerf=[0,0.143,0,0.0769,0.077,0.150,0.255,0.364,0.667,1,0.923,1;0.0588,0.0588,0,0.0926,0.0588,0.313,0.516,0.535,0.870,0.925,0.979,1;0,0.0571,0.0294,0.0833,0.105,0.241,0.320,0.370,0.653,0.972,1,0.971];
+    for trainingStage=1:3
+        subplot(3,1,sampleContrastInd);
+        
+        %plot raw data as circles:
+        if plotDataPoints==1;
+            %specify the marker type, marker colour, marker size, and line
+            %style:
+            plot(testContrasts(sampleContrastInd,:),rovingPerf(trainingStage,:),'Marker','o','Color',lineCols{sampleContrastInd},'LineStyle','none','MarkerSize',5);
+        end
+        hold on
+        
+        %plot a fitted function:
+        X=fminsearch(@fit_weibull,X0,[],testContrasts(sampleContrastInd,:),rovingPerf(trainingStage,:),[],'least_square',[1 0 1 0],[20 0 max(rovingPerf(trainingStage,:)) 0],[1 0 0 0],[0 0 0 0]);
+        xvals=0:1:testContrast(end)+10;
+        yvals=1-X(4)-X(3).*exp(-(xvals./X(2)).^X(1));
+        plot(xvals,yvals,'Color',lineCols{sampleContrastInd},'LineStyle',trainingStageLineStyles{trainingStage});
+        
+        %plot a straight line:
+        PSE=X(2).*(-log((0.5-X(4))/X(3))).^(1/X(1));
+        plot([PSE PSE],[0 1],'Color',lineCols{sampleContrastInd},'LineStyle',trainingStageLineStyles{trainingStage});
+        
+        %specify axis limits:
+        if truncateXLim==1
+            xlim([0 65]);
+        end
+        
+        %remove axes to the right & on top:
+        set(gca,'box','off');
+    end
+end
+subplot(3,1,1);
+title('Figure title');
+xlabel('Contrast');
+ylabel('Performance');
+%To save, use File > Export Setup.
+%Check requirements with journal or other publication outlets: image
+%formats, sizes, colour mode.
+%Specify desired settings, save template for repeated use.
+
+
+%
+%creating figures with transparency in Matlab:
+fig2=figure('Color',[1,1,1],'Units','Normalized','Position',[0.12, 0.08, 0.3, 0.4]);%'Position': left, bottom, width, height
+%create some data to plot:
+mean1=30;
+mean2=50;
+sigma=10;
+y1=normrnd(mean1,sigma,500,1);%500 normally distributed values with a mean of mean1 and an SD of sigma
+y2=normrnd(mean2,sigma,500,1);%500 normally distributed values with a mean of mean2 and an SD of sigma
+%set bin centres:
+bins=0:1:100;
+%generate histogram for first data set, y1:
+hist(y1,bins);
+%get the handle to the patch object that creates the histogram bar plot:
+h = findobj(gca,'Type','patch');
+%remove edge colour (automaticaly set to black):
+set(h,'EdgeColor','none')
+%set transparency (to 0.5):
+set(h,'FaceColor','r','facealpha',0.5)
+hold on
+%do the same for y2:
+hist(y2,bins);
+h = findobj(gca,'Type','patch');
+set(h,'facealpha',0.5,'EdgeColor','none')
+%set x-axis limits
+xlim([0 80]);
+%remove axes to the right & on top:
+set(gca,'box','off');
+%export figure as an svg file
+plot2svg
+
+%
+%creating figures and adjusting transparency in Adobe Illustrator:
+fig2=figure('Color',[1,1,1],'Units','Normalized','Position',[0.12, 0.08, 0.3, 0.4]);%'Position': left, bottom, width, height
+%create some data to plot:
+mean1=30;
+mean2=50;
+sigma=10;
+y1=normrnd(mean1,sigma,500,1);%500 normally distributed values with a mean of mean1 and an SD of sigma
+y2=normrnd(mean2,sigma,500,1);%500 normally distributed values with a mean of mean2 and an SD of sigma
+%set bin centres:
+bins=0:1:100;
+%generate histogram for first data set, y1:
+hist(y1,bins);
+h = findobj(gca,'Type','patch');
+set(h,'FaceColor',[0 .5 .5],'EdgeColor','none')
+hold on
+%do the same for y2:
+hist(y2,bins);
+h = findobj(gca,'Type','patch');
+set(h,'EdgeColor','none')
+%set x-axis limits
+xlim([0 80]);
+%remove axes to the right & on top:
+set(gca,'box','off');
+xlabel('x data');
+ylabel('y data');
+
+close all hidden
